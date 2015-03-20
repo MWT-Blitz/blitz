af854d70f1e0274dd221b2b5ed1b1e6c16084743
Mustaqeem Khowaja <mustaqeem.khowaja@gmail.com>
2015-03-19 23:38:52 -0400

    Displaying Hello World on the screen
    Creating a sprite batch and then using it to display the text.

--- core/src/com/blitz/game/screens/MainMenu.java
+++ core/src/com/blitz/game/screens/MainMenu.java
@@ -2,19 +2,29 @@ package com.blitz.game.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class MainMenu implements Screen{
-	
+	private SpriteBatch batch;
+    private BitmapFont font;
 	public Stage stage = new Stage();
 	
     @Override
     public void render(float delta) {
+    	batch = new SpriteBatch();    
+        font = new BitmapFont();
+        font.setColor(Color.BLUE);
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.act();
+        stage.act();	
         stage.draw();
+        batch.begin();
+        font.draw(batch, "Hello World", 200, 200);
+        batch.end();
     }
 
     @Override
@@ -40,6 +50,8 @@ public class MainMenu implements Screen{
 
    @Override
    public void dispose() {
+	   batch.dispose();
+       font.dispose();
        stage.dispose();
    }
 }
